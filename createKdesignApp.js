a8ccce5e8dbba51e31cbd32214ac6aeb14ff3e3e
hxh2010 <hongxh.work@gmail.com>
2024-03-15 15:23:17 +0800

    feat：v1.0.3

--- createKdesignApp.js
+++ createKdesignApp.js
@@ -2,10 +2,10 @@
 
 require('colors');
 const fs = require('fs');
+const path = require('path');
 const os = require('os');
 const child_process = require('child_process');
 const ora = require('ora');
-const remove = require('remove');
 const replace = require('replace-in-file');
 const inquirer = require('inquirer');
 
@@ -60,10 +60,24 @@ const cloneToTempDir = (clonePath, template) => {
   });
 };
 
+const deleteFolderRecursive = (folderPath) => {
+  if (fs.existsSync(folderPath)) {
+    fs.readdirSync(folderPath).forEach((file, index) => {
+      const curPath = path.join(folderPath, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(folderPath);
+  }
+};
+
 const editFileAndDelGit = async (projectName, downloadPath, template) => {
   const pwd = process.cwd();
-  const splitFlag = os.type() === 'Windows_NT' ? '\\' : '/'
-  const fullDownloadPath = pwd + '/' + downloadPath;
+  const splitFlag = os.type() === 'Windows_NT' ? '\\' : '/';
+  const fullDownloadPath = pwd + splitFlag + downloadPath;
 
   const options = {
     files: [fullDownloadPath + `${splitFlag}**${splitFlag}*`],
@@ -72,9 +86,10 @@ const editFileAndDelGit = async (projectName, downloadPath, template) => {
     to: projectName,
   };
   await replace(options);
-  remove.removeSync(fullDownloadPath + `${splitFlag}.git`);
-  remove.removeSync(fullDownloadPath + `${splitFlag}.github`);
+  deleteFolderRecursive(`${fullDownloadPath}${splitFlag}.git`);
+  deleteFolderRecursive(`${fullDownloadPath}${splitFlag}.github`);
 };
+
 
 const clone = (answers) => {
   console.log(`Initializing the KDesign App`.green);
@@ -82,24 +97,32 @@ const clone = (answers) => {
   const start = Date.now();
   spinner.start();
   spinner.color = 'green';
+
   cloneToTempDir(projectName, template).then(downloadPath => {
     spinner.text = `Receiving objects stage ${`100%`.green} complete`;
-    editFileAndDelGit(projectName, downloadPath, template).then(() => {
-      spinner.succeed();
-      console.log('');
-      console.log(`cd ${projectName} && ${template === 'umi3' ? 'npm install' : 'pnpm install'}`.gray);
-      console.log('');
-      console.log('Initializing Done in '.green + `${((Date.now() - start) / 1000).toFixed(1)}`.yellow + 's'.green);
-    }).catch(() => {
-      spinner.error();
-    });
-  }).catch(() => {
-    spinner.error();
+    editFileAndDelGit(projectName, downloadPath, template)
+      .then(()=>{
+        spinner.succeed();
+      })
+      .catch(() => {
+        spinner.succeed();
+        console.log('');
+        console.log('You should delete the .git manually.');
+      })
+      .finally(() => {
+        console.log('');
+        console.log(`cd ${projectName} && ${template === 'umi3' ? 'npm install' : 'pnpm install'}`.gray);
+        console.log('');
+        console.log('Initializing Done in '.green + `${((Date.now() - start) / 1000).toFixed(1)}`.yellow + 's'.green);
+      });
+  }).catch((err) => {
+    spinner.stop();
+    console.log('Initializing Error：', err);
   });
 };
 
 const create = (projectName = '', version = '') => {
-  console.log(`${version}`.yellow);
+  console.log(`current version：v${version}`.gray);
   if (!['-V', '--version'].includes(projectName)) {
     inquirer
       .prompt([
@@ -125,9 +148,9 @@ const create = (projectName = '', version = '') => {
           name: 'template',
           message: 'Which template do you need?',
           choices: [
-            {name: '1.umi3（使用umi3为基础框架，兼容ie11）', value: 'umi3'},
-            {name: '2.umi4（使用umi4为基础框架，不兼容ie11）', value: 'umi4'},
-            {name: '3.simple（使用umi4为基础框架，并简化典型页面及功能）', value: 'simple'},
+            {name: '1. umi3（使用umi3为基础框架，兼容ie11）', value: 'umi3'},
+            {name: '2. umi4（使用umi4为基础框架，不兼容ie11）', value: 'umi4'},
+            {name: '3. simple（使用umi4为基础框架，并简化典型页面及功能）', value: 'simple'},
           ],
           filter(val) {
             return val.toLowerCase();
